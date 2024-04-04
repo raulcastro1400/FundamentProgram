7adc6399e03916d5129444e65451aa0dc5c99aeb
Raul_Mac <castrochoqueraul@gmail.com>
2024-04-04 13:16:56 -0500

    Agregand Archivo funcion

--- 2. SOLUCION A EJERCICIOS/ejercicio1.js
+++ 2. SOLUCION A EJERCICIOS/ejercicio1.js
@@ -3,12 +3,41 @@ de una persona a la cual solo se le solicitará
 el año en que nació.*/
 
 
-let añoActual = 2024
-let AñoQueNAcio = 1989
-let edad
+//let añoActual = 2024
+//let AñoQueNAcio = 1989
+//let edad
 
-edad = añoActual-AñoQueNAcio
+//edad = añoActual-AñoQueNAcio
 
-console.log(edad)
+//console.log(edad)
 
+
+
+
+
+// function Nombre(Argunmentos){
+//    Intrucciones}
+
+function calcularEdad(){
+ 
+    while(true){
+        let opciones
+        opciones = prompt("¿Que desea hacer? \n1. Calcular Edad \n2. Salir  ")
+
+        if (opciones === "2"){
+            alert("Hasta Luego, que te valla bien, que te pise un tren")
+            break
+        }
+
+
+        //Vamos hallar la edad apartir de aqui
+
+        if(opciones === "1"){
+            let añoActual = parseInt(prompt("Por favor ingrese el año Actual"))
+            let añoNacimiento = parseInt(prompt("Por favor ingrese el año de su nacimiento"))
+            let edad = añoActual - añoNacimiento
+            alert("Su edad es "+ edad + " Años")
+        }
+    }
+}
 
